4957faafc67ddc2923ca973f12deee55b2485990
Junior Andrade <115849218+WJuniorAndrade@users.noreply.github.com>
2023-06-26 23:37:36 -0300

    Carrossel de Atendimentos
    Finalizado o layout do carrossel onde o cliente irá selecionar o atendimento que deseja realizar.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,16 +1,17 @@
 var horas = document.querySelector("#horas-vagas")
 var data = document.querySelector("#data-servico")
-var atend =  document.getElementsByName("atendimento")
+var atend =  document.getElementById("manicure")
 var enviar = document.getElementById("enviar")
+var nome = document.querySelector("#nome-cliente")
 
 enviar.addEventListener("click", function (event) {
     event.preventDefault()
-    console.log(horas.value);
+    /* console.log(horas.value);
 
     console.log(data.value);
 
-
-    console.log(atend);
+    console.log(nome.value); */
+    console.log(atend.value);
 
 
 
