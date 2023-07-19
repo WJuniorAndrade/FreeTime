01429b668357dd6d0f8f7165f3798d85974002fc
Junior Andrade <115849218+WJuniorAndrade@users.noreply.github.com>
2023-07-18 22:41:06 -0300

    Typescript e Grid
    Atualização do layout com display grid nas opções de atendimentos. Inicialmente o projeto tinha somente a linguagem Javascript porém, para uma melhor  estruturação do projeto foi implementado sua extensão Typescript.

---
+++ app/script.ts
@@ -0,0 +1,22 @@
+var horas = document.querySelector("#horas-vagas")
+var data = document.querySelector("#data-servico")
+var atend =  document.getElementById("manicure")
+var enviar = document.getElementById("enviar")
+var nome = document.querySelector("#nome-cliente")
+
+const nu1 = document.querySelector("#nu1");
+
+enviar.addEventListener("click", function (event) {
+    event.preventDefault()
+
+   
+    /* console.log(horas.value);
+
+    console.log(data.value);
+
+    console.log(nome.value); */
+    console.log(atend);
+
+
+    nu1.classList.add("nu1")
+})
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,16 @@
-var horas = document.querySelector("#horas-vagas")
-var data = document.querySelector("#data-servico")
-var atend =  document.getElementById("manicure")
-var enviar = document.getElementById("enviar")
-var nome = document.querySelector("#nome-cliente")
-
-const nu1 = document.querySelector("#nu1")
-
+var horas = document.querySelector("#horas-vagas");
+var data = document.querySelector("#data-servico");
+var atend = document.getElementById("manicure");
+var enviar = document.getElementById("enviar");
+var nome = document.querySelector("#nome-cliente");
+const nu1 = document.querySelector("#nu1");
 enviar.addEventListener("click", function (event) {
-    event.preventDefault()
+    event.preventDefault();
     /* console.log(horas.value);
 
     console.log(data.value);
 
     console.log(nome.value); */
-    console.log(atend.value);
-
-
-    nu1.classList.add("nu1")
-})
+    console.log(atend);
+    nu1.classList.add("nu1");
+});
